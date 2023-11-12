10e3898b55a11a6961707728a586d506993129e1
David Blundell <dcblundell@gmail.com>
2023-11-12 15:20:25 -0500

    config updates

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    readonly VITE_CURRENT_WEATHER_API: string;
+    readonly VITE_FORECAST_WEATHER_API: string;
+    readonly VITE_OPENWEATHERMAP_API_KEY: string;
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv
+}
